c0c788a8414f4ca22086c7cea39a48577a3f8a13
Sofía Mora <sofiamoramunoz6@gmail.com>
2022-03-17 11:21:08 -0600

    create-components

---
+++ project-1/src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: [
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private AuthService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+}
